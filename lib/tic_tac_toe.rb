1a0e35cf8885f264191d0998ddbc0e6f565de77f
Nicole Rothenay <nicoler@iccreativeagency.com>
2019-04-14 01:36:19 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -40,7 +40,7 @@ end
     puts "Please enter 1-9:"
   input = gets.strip
   index = input_to_index(input)
-  current_player = current_player
+
   if valid_move?(index)
     move(index, current_player)
     display_board
